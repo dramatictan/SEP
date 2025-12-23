29d98f66f4e5f074dd49a529c8d2a755cf2999ee
Tan Guan Ying <guanying.23@ichat.sp.edu.sg>
2025-12-24 00:38:38 +0800

    style: adjust design of card showroom

--- view/B/js/loadPublicShowroom.js
+++ view/B/js/loadPublicShowroom.js
@@ -12,23 +12,34 @@ document.addEventListener('DOMContentLoaded', loadAllShowrooms);
     }
 
     function renderShowrooms(showrooms) {
+        // select grid container by id
         const grid = document.getElementById('showroomGrid');
         grid.innerHTML = '';
 
-        showrooms.forEach(showroom => {
+        // loop through showrooms
+        showrooms.forEach((showroom, index) => {
+
+            // every 5th card is made larger to make it interesting instead of boring card layouts
+            const isLarge = index % 5 === 0;
+
+            // limit description length to prevent breaking thru the card, and replace the long text with '...'
+            const shortDesc = showroom.description
+                ? showroom.description.substring(0, 90) + '...'
+                : '';
+
             grid.innerHTML += `
-                <div class="col-md-4">
-                    <div class="card showroom-card h-100 border-0 shadow-sm">
+                <div class="${isLarge ? 'col-md-8' : 'col-md-4'}">
+                    <div class="card showroom-card h-100 border-0 shadow-sm ${isLarge ? 'large-card' : ''}">
 
                         <div class="image-wrapper">
-                            <img 
-                                src="${showroom.cover_image_url}" 
-                                alt="${showroom.name}">
+                            <img src="${showroom.cover_image_url}" alt="${showroom.name}">
                             <span class="category-badge">${showroom.category_name}</span>
                         </div>
 
                         <div class="card-body d-flex flex-column">
                             <h5 class="card-title">${showroom.name}</h5>
+
+                            ${!isLarge ? `<p class="card-text text-muted small-desc">${shortDesc}</p>` : ''}
 
                             <a 
                                 href="showroomDetail.html?id=${showroom.id}" 
@@ -39,5 +50,6 @@ document.addEventListener('DOMContentLoaded', loadAllShowrooms);
                     </div>
                 </div>
             `;
-        });
+        }
+    );
 }
