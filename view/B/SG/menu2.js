8437972d467997d9cba70c76dee34cd6c9815da7
THYIgnatius <IGNATIUSTEO.24@ichat.sp.edu.sg>
2025-12-28 10:45:26 +0800

    moving files

--- view/B/SG/menu2.js
+++ view/B/SG/menu2.js
@@ -90,6 +90,9 @@ document.write('\
                     <li>\
                         <a href = "/B/' + countryPrefix + '/showroomHome.html">Showroom</a>\
                     </li>\
+                    <li>\
+                        <a href = "/B/' + countryPrefix + '/promotion.html">Showroom</a>\
+                    </li>\
                     <li class="dropdown">\
                         <a class="dropdown-toggle" href="#">\
                             Furniture <i class="icon icon-angle-down"></i>\
